2c543c38bd06ea608595e576145bc78e95739a71
tizzifona <tizzifona@gmail.com>
2025-05-08 14:29:10 +0200

    Added code for exercise Calculator

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,13 @@ public class Calculator {
     public static void main(String[] args) {
 
         //Ejecuta aquí todos los métodos creados e imprime en la terminal. 
-        
+        double a = 15;
+        double b = 5;
+
+        System.out.println("Addition: " + add(a, b));
+        System.out.println("Subtraction: " + subtract(a, b));
+        System.out.println("Multiplication: " + multiply(a, b));
+        System.out.println("Division: " + divide(a, b));
     }
 
     //Define los métodos basándote en su doc comment.
@@ -18,7 +24,9 @@ public class Calculator {
      * Inside the function:
      * 1. sum 2 numbers
      */
-    
+    public static double add(double number1, double number2) {
+        return number1 + number2;
+    }
 
     /**
      * Function name: subtract
@@ -30,7 +38,9 @@ public class Calculator {
      * Inside the function:
      * 1. subtract 2 numbers
      */
-    
+    public static double subtract(double number1, double number2) {
+        return number1 - number2;
+    }
 
     /**
      * Function name: multiply
@@ -42,7 +52,9 @@ public class Calculator {
      * Inside the function:
      * 1. multiply 2 numbers
      */
-    
+    public static double multiply(double number1, double number2) {
+        return number1 * number2;
+    }
 
     /**
      * Function name: divide
@@ -54,6 +66,7 @@ public class Calculator {
      * Inside the function:
      * 1. divide 2 numbers
      */
-    
-
+    public static double divide(double number1, double number2) {
+        return number1/number2;
+    }
 }
