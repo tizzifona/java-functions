37862a6d35ad498d6e73353552f28485778f61d1
tizzifona <tizzifona@gmail.com>
2025-05-08 13:55:45 +0200

    Added code for exercise Welcome

--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,30 @@
 //Inicializa la clase Welcome
+
+public class Welcome {
+
 //Crea el método main
+
+    public static void main(String[] args) {
+        greeting();
+        greetingCoder("Nadiia");
+    }
 
 //Tarea 1
 //Define el método greeting que imprima en terminal ¡Hola FemCoder!
-//Ejecuta el método greeting 
+//Ejecuta el método greeting
+
+    public static void greeting() {
+        System.out.println("¡Hola FemCoder!");
+    }
 
 //Tarea 2
 //Define el método greetingCoder que reciba un nombre y que imprima en terminal ¡Hola <name>!
-//Ejecuta el método greetingCoder 
+//Ejecuta el método greetingCoder
+
+    public static void greetingCoder(String name) {
+        System.out.println("¡Hola " + name + "!");
+    }
 
 //¿Sabes donde definir el método y en dónde lo has de ejecutar? Si no lo sabes vuelve al archivo de Resources.md
+    
+}
